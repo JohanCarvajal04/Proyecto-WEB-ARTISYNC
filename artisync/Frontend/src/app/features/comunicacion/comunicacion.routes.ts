3ef29a5ae90cd250b687c092055023dca3c3b714
Jhon-Kevin-Rios-Cuyabazo <JhonRios_180@hotmail.com>
2026-08-15 15:28:50 -0500

    feat(comunicacion): configurar sistema de ruteo del modulo

--- artisync/Frontend/src/app/features/comunicacion/comunicacion.routes.ts
+++ artisync/Frontend/src/app/features/comunicacion/comunicacion.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { BriefingFormComponent } from './pages/briefing-form/briefing-form.component';
+import { SalaChatComponent } from './pages/sala-chat/sala-chat.component';
 
 export const COMUNICACION_ROUTES: Routes = [
-  // TODO: Add routes
+  {
+    path: 'briefing/:idPedido',
+    component: BriefingFormComponent,
+    title: 'Completar Briefing'
+  },
+  {
+    path: 'chat/:idPedido',
+    component: SalaChatComponent,
+    title: 'Sala de Chat'
+  }
 ];
