c0ac2265168ac5e5c7cf966830cf56723c506fa2
Johan_Loor <91645452+JohanCarvajal04@users.noreply.github.com>
2026-09-05 23:42:36 -0500

    Update RespuestaSubcategoria.java

--- artisync/Backend/src/main/java/uteq/edu/ec/artisync/dto/respuesta/catalogo/RespuestaSubcategoria.java
+++ artisync/Backend/src/main/java/uteq/edu/ec/artisync/dto/respuesta/catalogo/RespuestaSubcategoria.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class RespuestaSubcategoria {
+public class RespuestaSubcategoria implements Serializable {
 
     private Long idSubcategoria;
     private Long idCategoria;
