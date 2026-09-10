759302ad9cc5ebf148e64bc3d56927f95bfb994e
Bryan Figueroa <bfigueroam@uteq.edu.ec>
2026-09-10 10:59:10 -0500

    Revert "feat(sistema,administracion): modulo de respaldo automatizado de base de datos (REQ-NF-024)"
    This reverts commit fd3ba6480314e8d2fea680e3652a50e239a0fb16.

--- artisync/Backend/src/main/java/uteq/edu/ec/artisync/repository/sistema/RespaldoPoliticaRepository.java
+++
@@ -1,9 +0,0 @@
-package uteq.edu.ec.artisync.repository.sistema;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uteq.edu.ec.artisync.entity.sistema.RespaldoPolitica;
-
-@Repository
-public interface RespaldoPoliticaRepository extends JpaRepository<RespaldoPolitica, Integer> {
-}
